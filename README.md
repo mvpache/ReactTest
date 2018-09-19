d81a1bb95c5b9c53473b21e23cbec33fa2a06646
mvpache <mvpache@msn.com>
2018-09-19 10:06:57 -0700

    intial get request set up

--- README.md
+++ README.md
@@ -5,3 +5,19 @@ https://data.cityofnewyork.us/api/views/25th-nujf/rows.json
  Filters (Year, Gender and Ethnicity) must be dynamically populated from the data.  
  Filters should include an “All” option.  
  Show applied filters in a separate component   
+
+
+0:30111  
+1:"C1155C27-19FD-4158-A3C4-DEF3423BE8BD"  
+2:30111  
+3:1534519451  
+4:"399231"  
+5:1534519451  
+6:"399231"  
+7:null  
+8:"2011" - BIRTHYEAR  
+9:"FEMALE" - GENDER  
+10:"ASIAN AND PACIFIC ISLANDER" - ETHNICITY  
+11:"CHLOE" - NAME  
+12:"106" - NUMBER WITH THIS NAME   
+13:"2" - RANK
--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://data.cityofnewyork.us/api/views/25th-nujf/rows.json`)
+      .then(names => this.setState({ names: names.data.data.slice(0, 10) }))
+      .catch();
+  }
   render() {
     return (
-      <div className="App">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        To get started, edit <code>src/App.js</code> and save to reload.
       </div>
     );
   }
